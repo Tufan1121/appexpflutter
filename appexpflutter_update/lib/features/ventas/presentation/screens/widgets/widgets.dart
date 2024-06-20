ec3052ebc99584121fc226d416faa72b168e0f03
Rubén Darío González <71092701+gonzalezruben681@users.noreply.github.com>
2024-06-19 22:11:51 -0600

    add: readireccionar custon dropdown

--- appexpflutter_update/lib/features/ventas/presentation/screens/widgets/widgets.dart
+++ appexpflutter_update/lib/features/ventas/presentation/screens/widgets/widgets.dart
@@ -1,4 +1,4 @@
 export 'cliente_form.dart';
 export 'search_clientes.dart';
 export 'cliente_form_edit.dart';
-export 'custom_dropdownbutton.dart';
+export '../../../../shared/widgets/custom_dropdownbutton.dart';
