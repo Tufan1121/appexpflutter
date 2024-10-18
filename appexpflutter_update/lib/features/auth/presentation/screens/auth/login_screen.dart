0b735f8b8f6ed6385bbeb1e3f6a1d16be7217d5e
Rubén Darío González <71092701+gonzalezruben681@users.noreply.github.com>
2024-10-18 14:21:09 -0600

    add: ajustes

--- appexpflutter_update/lib/features/auth/presentation/screens/auth/login_screen.dart
+++ appexpflutter_update/lib/features/auth/presentation/screens/auth/login_screen.dart
@@ -7,7 +7,6 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    final scaffoldBackgroundColor = Theme.of(context).scaffoldBackgroundColor;
 
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
