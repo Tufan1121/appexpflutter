cdd90e6bd6755e1429971af33e2da3c580aaff21
Rubén Darío González <71092701+gonzalezruben681@users.noreply.github.com>
2024-10-28 23:23:10 -0600

    add: ajustes para el tiempo de sesión y eliminado del token

--- appexpflutter_update/lib/my_app.dart
+++ appexpflutter_update/lib/my_app.dart
@@ -1,3 +1,11 @@
+import 'dart:async';
+import 'package:appexpflutter_update/features/auth/config/config_token.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:appexpflutter_update/main.dart';
+import 'package:appexpflutter_update/config/config.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:appexpflutter_update/features/galeria/presentation/blocs/detalle_galeria/detalle_galeria_bloc.dart';
 import 'package:appexpflutter_update/features/galeria/presentation/blocs/detalle_producto/detalle_producto_bloc.dart';
 import 'package:appexpflutter_update/features/galeria/presentation/blocs/galeria/galeria_bloc.dart';
@@ -6,15 +14,6 @@ import 'package:appexpflutter_update/features/punto_venta/presentation/blocs/con
 import 'package:appexpflutter_update/features/punto_venta/presentation/blocs/inventario_tienda/inventario_tienda_bloc.dart';
 import 'package:appexpflutter_update/features/punto_venta/presentation/blocs/pedido/pedido_bloc.dart';
 import 'package:appexpflutter_update/features/punto_venta/presentation/blocs/producto/productos_tienda_bloc.dart';
-import 'package:appexpflutter_update/features/reportes/presentation/bloc/reportes_bloc.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:appexpflutter_update/main.dart';
-import 'package:appexpflutter_update/config/config.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:appexpflutter_update/features/historial/presentation/blocs/historial/historial_bloc.dart';
-import 'package:appexpflutter_update/features/historial/presentation/blocs/sesion/sesion_bloc.dart';
 import 'package:appexpflutter_update/features/inventarios/presentation/blocs/busqueda_global/busqueda_global_bloc.dart';
 import 'package:appexpflutter_update/features/inventarios/presentation/blocs/inventario_bodega/inventario_bodega_bloc.dart';
 import 'package:appexpflutter_update/features/inventarios/presentation/blocs/inventario_expo/inventario_expo_bloc.dart';
@@ -27,6 +26,8 @@ import 'package:appexpflutter_update/features/ventas/presentation/blocs/producto
 import 'package:appexpflutter_update/features/precios/presentation/bloc/precios_bloc.dart';
 import 'package:appexpflutter_update/features/auth/presentation/bloc/auth_bloc.dart';
 
+import 'features/historial/presentation/blocs/historial/historial_bloc.dart';
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -34,17 +35,70 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   late final GoRouter _router;
   final storage = const FlutterSecureStorage();
+  Timer? sessionTimer;
+  final configToken = ConfigToken();
+
+  StreamSubscription? sessionStream;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _init();
+  }
 
   Future<void> _init() async {
     final token = await storage.read(key: 'accessToken');
-    // _router =
-    //     GoRouter(initialLocation: PruebsRoute.path, routes: $appRoutes);
+
+    // Inicializa el router con la ruta de inicio de sesión o la ruta principal.
     _router = GoRouter(
-        initialLocation: token != null ? HomeRoute.path : LoginRoute.path,
-        routes: $appRoutes);
+      initialLocation: token != null ? HomeRoute.path : LoginRoute.path,
+      routes: $appRoutes,
+    );
+
+    // Inicia el Stream de verificación de sesión si el token existe
+    if (token != null) {
+      _startSessionStream();
+    }
+  }
+
+  // Inicia un Stream que verifica cada minuto si han pasado 10 minutos
+  void _startSessionStream() {
+    sessionStream =
+        Stream.periodic(const Duration(minutes: 1)).listen((_) async {
+      final hasExpired = await configToken.checkAndDeleteTokenIfNeeded();
+      if (hasExpired) {
+        _redirectToLogin();
+      }
+    });
+  }
+
+  // Redirige al usuario a la pantalla de inicio de sesión
+  void _redirectToLogin() {
+    sessionStream?.cancel(); // Cancela el stream al cerrar sesión
+    _router.go(LoginRoute.path);
+  }
+
+  // Detener el Stream al salir de la app o al pausar
+  @override
+  void dispose() {
+    sessionStream?.cancel(); // Cancela el stream si se destruye el widget
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  // Detecta cuando la app pasa a primer o segundo plano y reinicia el stream
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed && sessionStream == null) {
+      _startSessionStream();
+    } else if (state == AppLifecycleState.paused) {
+      sessionStream?.cancel();
+      sessionStream = null;
+    }
   }
 
   @override
@@ -62,6 +116,7 @@ class _MyAppState extends State<MyApp> {
             create: (_) => injector<SesionPedidoBloc>()),
         BlocProvider<CotizaPedidoBloc>(
             create: (_) => injector<CotizaPedidoBloc>()),
+        BlocProvider<HistorialBloc>(create: (_) => injector<HistorialBloc>()),
         BlocProvider<InventarioBloc>(create: (_) => injector<InventarioBloc>()),
         BlocProvider<InventarioTiendaBloc>(
             create: (_) => injector<InventarioTiendaBloc>()),
@@ -75,10 +130,6 @@ class _MyAppState extends State<MyApp> {
             create: (_) => injector<ProductosTiendaBloc>()),
         BlocProvider<BusquedaGlobalBloc>(
             create: (_) => injector<BusquedaGlobalBloc>()),
-        BlocProvider<HistorialBloc>(create: (_) => injector<HistorialBloc>()),
-        BlocProvider<DetalleSesionBloc>(
-            create: (_) => injector<DetalleSesionBloc>()),
-        BlocProvider<ReportesBloc>(create: (_) => injector<ReportesBloc>()),
         BlocProvider<GaleriaBloc>(create: (_) => injector<GaleriaBloc>()),
         BlocProvider<DetalleGaleriaBloc>(
             create: (_) => injector<DetalleGaleriaBloc>()),
