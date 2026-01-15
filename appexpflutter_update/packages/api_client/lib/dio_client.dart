3cecc184d74abcc13c39c409dd2d2a512c087f56
hheisego <centos.heise@gmail.com>
2026-01-15 13:52:22 -0600

    feat: Implement a Dio-based API client with common HTTP methods for network requests.

--- appexpflutter_update/packages/api_client/lib/dio_client.dart
+++ appexpflutter_update/packages/api_client/lib/dio_client.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:api_client/constants/environment.dart';
 import 'package:api_client/exceptions/custom_exceptions/not_found_expection.dart';
 import 'package:dio/dio.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
+
 
 class DioClient {
   late final Dio _dio;
@@ -21,30 +21,9 @@ class DioClient {
       ..options.responseType = ResponseType.json;
   }
 
-  Future<bool> _hasInternetConnection() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult.contains(ConnectivityResult.none)) {
-      return false;
-    } else {
-      try {
-        final result = await InternetAddress.lookup('google.com');
-        return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
-      } catch (e) {
-        return false;
-      }
-    }
-  }
-
   Future<Response<dynamic>> _request(
     Future<Response<dynamic>> Function() requestFunction,
   ) async {
-    bool hasConnection = await _hasInternetConnection();
-    if (!hasConnection) {
-
-      throw NotFoundException(
-          'No hay conexión a Internet. Por favor, verifique su conexión.');
-    }
-
     try {
       return await requestFunction();
     } on DioException {
